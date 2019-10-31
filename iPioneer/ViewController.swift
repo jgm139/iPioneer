4ce41a31719bfe83560e299e4141a6a2d916dbaf
Julia García Martínez <jgm139@alu.ua.es>
2019-10-31 17:35:41 +0100

    Initial Commit

---
+++ iPioneer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iPioneer
+//
+//  Created by Máster Móviles on 31/10/2019.
+//  Copyright © 2019 Máster Móviles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iPioneerTests/iPioneerTests.swift
@@ -0,0 +1,34 @@
+//
+//  iPioneerTests.swift
+//  iPioneerTests
+//
+//  Created by Máster Móviles on 31/10/2019.
+//  Copyright © 2019 Máster Móviles. All rights reserved.
+//
+
+import XCTest
+@testable import iPioneer
+
+class iPioneerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
